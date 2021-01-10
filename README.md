70136a9b0840f75f012fb2c091025cbd6a33967e
Ruslan Borovilov <pycz2007@yandex.ru>
2021-01-10 17:41:46 +0300

    First version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # owa-mail-copy
 User script for easy copy of your e-mail adress in Outlook Web App
+
+## How it works:
+Pretty simple, you got this button to get you e-mail in clickboard by clicking on it:
+![Demo](img/screen.png "Demo")
+
+## Install guide
+1. Install [Tampermonkey](https://www.tampermonkey.net/) for browser of your choice.
+2. Install script by clicking on this [link](https://raw.githubusercontent.com/Pycz/owa-mail-copy/main/owa_mail_copy.user.js).
+3. ???
+4. PROFIT!
+
+## Feedback and contribution
+I didn't spend much time on this and this user script may be not perfect,
+but it works for me and
+all feedback and contribution is appreciated.
+Feel free to make issues and pull requests!
---
+++ owa_mail_copy.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name        Owa mail copy
+// @version     0.0.1
+// @description Add button for easy copy of e-mail address in OWA
+// @author      Ruslan Borovilov
+// @namespace   https://github.com/Pycz/
+// @homepageURL https://github.com/Pycz/owa-mail-copy
+// @match       *://*/owa/*
+// @match       https://outlook.live.com/mail/*
+// @match       https://outlook.office365.com/mail/*
+// @match       https://outlook.office.com/mail/*
+// @grant       none
+// ==/UserScript==
+
+
+(function(){
+    let COPY_BUTTON_ID = 'pycz_copy_button';
+    setTimeout(() => {
+        let user_button = document.getElementsByClassName('_hl_e')[0];
+        user_button.onclick = (e) => {
+            setTimeout(() => {
+                let popup = document.getElementsByClassName('headerMenuDropShadow')[0];
+                let email_adress = popup.getElementsByClassName('_pe_I')[0];
+                let to_copy = email_adress.textContent;
+
+                // add button
+                let copy_button = document.createElement('span');
+                copy_button.id = COPY_BUTTON_ID;
+                copy_button.innerText = '⬇';
+                copy_button.style.border = '1px solid black';
+                copy_button.style.marginLeft = '5px';
+                copy_button.style.paddingLeft = '2px';
+                copy_button.style.paddingRight = '1px';
+                copy_button.onclick = (e) => {
+                    navigator.permissions.query({name: "clipboard-write"}).then(result => {
+                        if (result.state == "granted" || result.state == "prompt") {
+                            navigator.clipboard.writeText(to_copy).then(() => {
+                                console.log(`Copy '${to_copy}' is succsseed!`);
+                            }, () => {
+                                console.log('Copy error!');
+                            });
+                        }
+                        else {
+                            console.log('No permissions!');
+                        }
+                    });
+                };
+
+                email_adress.appendChild(copy_button);
+
+            }, 500)
+        }
+    }, 1000)
+})();
